7b923e5edb0b0e347a18270859c7763ecefbe019
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-21 15:33:53 -0400

    update index attribution meta
    switch from function to class based component

--- practice/practice/src/Components/Hello.js
+++ practice/practice/src/Components/Hello.js
@@ -1,26 +1,22 @@
 import React from "react";
 // import ReactDOM from "react-dom";
 
-// class Hello extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Yow Yow</h1>
-//                 <h1>Yow Yow</h1>
-//                 <h1>Yow Yow</h1>
-//             </div>
-//         );
-//     }
-// }
-
-function Hello (){
-    return(
-          <div>
-            <h1>Yow Yow</h1>
-                <h1>Yow Yow</h1>
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
                 <h1>Yow Yow</h1>
             </div>
-    )
+        );
+    }
 }
+
+// function Hello (){
+//     return(
+//           <div>
+//             <h1>Yow Yow</h1>
+//             </div>
+//     )
+// }
 
 export default Hello;
