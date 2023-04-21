313f1d9db0b05fa91016ebc0bc297adc7db035fa
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-21 08:39:17 -0400

    Create Hello component and render

--- practice/practice/src/App.js
+++ practice/practice/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Hello from './Components/Hello';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>YOW YOW</h1>
+     <Hello />
     </div>
   );
 }
---
+++ practice/practice/src/Components/Hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+
+class Hello extends React.Component {
+    render() {
+        return( <h1>Yow Yow</h1>);
+    }
+}
+
+export default Hello;
