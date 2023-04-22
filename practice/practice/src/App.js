35d516d73a570266029d455dcbc711e83dbfce3f
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-22 09:37:32 -0400

    add number picker

--- practice/practice/src/App.js
+++ practice/practice/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hello from './Components/Hello';
 import React from 'react';
+import NumPicker from './Components/NumPicker';
 
 function App() {
   return (
     <div className="App">
      <Hello />
+     <NumPicker />
     </div>
   );
 }
