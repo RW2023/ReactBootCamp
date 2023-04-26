c5723400f74158822e619527e8e85070ed48c182
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-04-26 02:04:07 -0400

    create slot machine

--- practice/practice/src/App.js
+++ practice/practice/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Hello from './Components/Hello';
 import React from 'react';
 import NumPicker from './Components/NumPicker';
+import Machine from './Components/Machine';
 
 function App() {
   return (
     <div className="App">
      <Hello />
      <NumPicker />
+     <Machine
+     s1='🍊' 
+        s2='🍒' 
+        s3='🍒' 
+     />
     </div>
   );
 }
--- practice/practice/src/Components/Machine.js
+++ practice/practice/src/Components/Machine.js
@@ -1,15 +1,17 @@
+import React from 'react'
 class Machine extends React.Component {
-    s1 = "🍓"
-    s2 ="🍉"
-    s3="🍑"
+    s1 = "🍒"
+    s2 ="🍊"
+    s3 ="🍏"
     render() {
         const { s1, s2, s3 } = this.props;
         const winner = (s1 === s2) && (s2 === s3);
 
         return (
             <div>
+                <h1>Slot Machine</h1>
                 <p>{s1} {s2} {s3}</p>
-                <p>{winner ? 'Winner!' : 'Loser!'}</p>
+                <p>{winner ? 'winner!' : 'Loser!'}</p>
             </div>
         )
     }
