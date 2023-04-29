e9e3a08b8b20cc735734c49ca178d0bc5402fe86
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-04-29 11:37:11 -0400

    update app component to now render pokedex
    remove pokicard component and replace with a single Pokedex component with no props

--- react-bootcamp-materials/05-pokedex-project/pokedex/src/App.js
+++ react-bootcamp-materials/05-pokedex-project/pokedex/src/App.js
@@ -1,27 +1,12 @@
 
-import PokeCard from './Components/PokeCard/PokeCard';
+
 import PokeDex from './Components/Pokedex/Pokedex';
 
 
 function App() {
   return (
     <div className="App">
-  
-      <PokeCard
-      id={4}
-      name='Charmander'
-      type="fire"
-      Experience={62}
-      />
-      <PokeCard
-        id={9}
-        name='Baldazore'
-        type="earth"
-        Experience={77}
-      />
-      <PokeDex/>
-
-      
+      <PokeDex />
     </div>
   );
 }
