3a5148a7f41d5598f4fc70bbaeb90b4f604d57f6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-04-30 15:25:26 -0400

    update to fancier api for images

--- react-bootcamp-materials/05-pokedex-project/pokedex/src/Components/PokeCard/PokeCard.js
+++ react-bootcamp-materials/05-pokedex-project/pokedex/src/Components/PokeCard/PokeCard.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import './PokeCard.css'
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-console.log(POKE_API)
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+
 class PokeCard extends Component {
 
     render() {
-        let imageSrc = `${POKE_API}${this.props.id}.png`;
+        let imageSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
 
         return (
             <div className="PokeCard">
